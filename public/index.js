0ced049b889cf6efbf35fa541e9f7ccb2338ba03
Luis Pedro Coelho <luis@luispedro.org>
2021-11-19 14:54:08 +0100

    ENH Do not fail when the twitter sidebar is absent

--- public/index.js
+++ public/index.js
@@ -5,11 +5,14 @@ export default {
   load: async function (elmLoaded) {
     const app = await elmLoaded;
     console.log("App loaded", app);
-    var sc = document.createElement('script');
-    sc.src = 'https://platform.twitter.com/widgets.js';
-    sc.setAttribute('async', true);
-    sc.setAttribute('charset', 'utf-8');
-    document.getElementById('twitter-injection-site').appendChild(sc);
+    var twt = document.getElementById('twitter-injection-site');
+    if (twt) {
+        var sc = document.createElement('script');
+        sc.src = 'https://platform.twitter.com/widgets.js';
+        sc.setAttribute('async', true);
+        sc.setAttribute('charset', 'utf-8');
+        twt.appendChild(sc);
+    }
 
     sc = document.createElement('script');
     sc.setAttribute('src', "https://www.googletagmanager.com/gtag/js?id=UA-176414600-1");
