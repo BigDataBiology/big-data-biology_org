ca5c5f77efaa219a1e8b7574943b9976b82b1b1d
Luis Pedro Coelho <luis@luispedro.org>
2022-12-30 09:27:49 +0800

    ENH Remove outdated 'Coming Up' sidebar

--- src/Shared.elm
+++ src/Shared.elm
@@ -115,7 +115,7 @@ view sharedData page model toMsg pageView =
                             (case pageView.sidebar of
                                 Just p -> [p]
                                 Nothing ->
-                                    [Html.h3 [] [Html.text "Coming up"]
+                                    [{-Html.h3 [] [Html.text "Coming up"]
                                     ,Html.div
                                         [HtmlAttr.style "border-left" "2px solid #333"
                                         ,HtmlAttr.style "padding-left" "0.5em"
@@ -130,7 +130,7 @@ view sharedData page model toMsg pageView =
                                             ,Html.a [HtmlAttr.href "https://bit.ly/2022-10-27-lpc-office-hours"]
                                                 [Html.text "Online office hours (open, but registration required). "]
                                             ]
-                                    ,Html.h3 [] [Html.text "Most recent paper"]
+                                    ,-}Html.h3 [] [Html.text "Most recent paper"]
                                     ,Html.div
                                         [HtmlAttr.style "border-left" "2px solid #333"
                                         ,HtmlAttr.style "padding-left" "0.5em"
